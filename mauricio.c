94dac71045803f66fb093ad8653dd16b19bcfd38
Beatriz Vieira <gomes.pessoa@escola.pr.gov.br>
2026-06-08 10:56:20 -0300

    Add files via upload

---
+++ mauricio.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    printf("***************************************************************\n");
+    printf("*       Bem vindo ao nosso jogo de descobrir números em C     *\n");
+    printf("***************************************************************\n");
+
+    int numero_secreto = 57;
+
+    printf(numero_secreto);
+
+
+
+
+
+
+
+
+
+
+
+
+}
